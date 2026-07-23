1b2373031a9b35cb908205369a66fabb74604c88
Aarushi <anu@Aarushis-MacBook-Pro.local>
2026-07-23 15:52:17 +0530

    Defualt border

--- lib/types.ml
+++ lib/types.ml
@@ -3,3 +3,13 @@ open Buffer
 type test_driver = {
     pos: int * int
 }
+type plain = {
+    top_left: string;
+    top_right: string;
+    bottom_left: string;
+    bottom_right: string;
+    vertical_left: string;
+    vertical_right: string;
+    horizontal_top: string;
+    horizontal_bottom: string
+}
--- lib/window.ml
+++ lib/window.ml
@@ -5,4 +5,30 @@ module Border = Set.Make(struct
   let compare a b = compare (String.lowercase_ascii a) (String.lowercase_ascii b)
 end)
 
+let default_window_style() =
+  let init_border_style = Border.empty |>
+    Border.add "│" |>
+
+    Border.add "─" |>
+
+    Border.add "┐" |>
+
+    Border.add "┌" |>
+
+    Border.add "┘" |>
+
+    Border.add "└" |>
+
+    Border.add "┤" |>
+
+    Border.add "├" |>
+    Border.add "┬" |>
+
+    Border.add "┴" |>
+
+    Border.add "┼"
+    in init_border_style
+
+
+
 end
